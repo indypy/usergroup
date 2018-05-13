28b762b41bfe9534d5072598414cfb09b6d1edf8
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2018-05-13 14:42:49 -0400

    reserving this name

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'usergroup',
+    version = '1.0',
+    url = 'https://github.com/indypy/usergroup.git',
+    author = 'Calvin Hendryx-Parker',
+    author_email = 'calvin@indypy.org',
+    description = 'Make a user group',
+    packages = find_packages(),
+    install_requires = [],
+)
